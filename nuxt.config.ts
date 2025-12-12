b256057b1abdf6bd718fdb1708fe556e2de5f44f
Tonn-nnay <tymartinmarin@gmail.com>
2025-12-12 10:14:46 +0100

    fix: vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
-  devtools: { 
-    enabled: true 
+  devtools: {
+    enabled: true,
   },
   css: ["primeicons/primeicons.css"],
   modules: ["@pinia/nuxt"],
   runtimeConfig: {
-    pokemonApiUrl: ''
-  }
+    pokemonApiUrl: "https://pokeapi.co/api/v2",
+  },
 });
